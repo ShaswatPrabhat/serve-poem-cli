5be41d1b9bc2616fe9d82d94bbec01d39c42ecb8
Shaswat Prabhat <spd1238@gmail.com>
2021-02-13 14:52:05 +0530

    Fix tests, add extension to xo file, move ui to src directory.

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import chalk from 'chalk';
 import test from 'ava';
 import {render} from 'ink-testing-library';
-import App from './ui';
+import App from './src/ui.js';
 
 test('greet unknown user', t => {
 	const {lastFrame} = render(<App/>);
